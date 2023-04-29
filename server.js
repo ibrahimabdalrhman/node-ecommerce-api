2038e29074959249a239f8a4ed7e349f16fc9aa7
ibrahim abdalrhman <59165733+ibrahimabdalrhman@users.noreply.github.com>
2023-04-29 16:49:14 +0300

    test

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const ApiError = require("./utils/apiError");
 const errorMiddleware = require('./middlewares/errorMiddleware');
 require('mongoose').set('strictQuery', false);
 const { webhookCheckout } = require('./services/orderService');
+const bodyParser = require("body-parser");
 
 const app = express();
 dbConnection();
